00619a39341d006b1d845a06cc81b2f036f339e7
Harshit Sonawala <hssonawala@gmail.com>
2022-02-15 21:52:04 +0530

    added useLocation and useNavigate with params

--- src/components/ImageDescription.js
+++ src/components/ImageDescription.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import '../App.css';
+import { ArrowBack } from '@material-ui/icons';
 
 const ImageDescription = () => {
-  const { state } = useLocation();
+  const { pathname, state } = useLocation();
   // const params = useParams();
+  const navigate = useNavigate();
 
   return (
     <div className='eightyPercentStyle'>
+      <div className='flexRowStyle'>
+        <button
+          onClick={() => { navigate(-1); }}
+        >
+          <ArrowBack style={{ fontSize: '22px' }} />
+        </button>
+        <p>Path: {pathname}</p>
+      </div>
       <h1>Image Description Page for {state}</h1>
     </div>
   );
